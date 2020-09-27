a6050ca38f8ffd4f278ec7ae0336a3a48c76ef0c
иван кузьмич <ivankuzmic210@gmail.com>
2020-09-27 15:24:15 +0300

    Coded merge sort

--- Laba5/Sort.cpp
+++ Laba5/Sort.cpp
@@ -1,40 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
-
+#include <vector>
 using namespace std;
 
-void generate_arr(int*, int);
-
-void out_arr(int* arr, int len);
-
-void insert_sort(int* arr, int len);
-
-void merge_sort(int* arr, int len);
-
-//sort insert and sort merge
-int main()
-{
-	srand(time_t(NULL));
-	int len;
-	cout << "Input size of array: ";
-	cin >> len;
-	int* arr = new int[len];
-
-	generate_arr(arr, len);
-
-	insert_sort(arr, len);
-
-	out_arr(arr, len);
-	cout << "\n\n";
-
-	//merge_sort(arr, len);
-
-	delete[] arr;
-	return 0;
-}
 // generate array
-void generate_arr(int* arr, int len)
+template<typename T>
+void generate_arr(T* arr, int len)
 {
 	int lower = 100, upper = 1000, step = 100;
 	for (int i = 0; i < len; i++)
@@ -46,7 +18,8 @@ void generate_arr(int* arr, int len)
 	cout << "\n";
 }
 //output array
-void out_arr(int* arr, int len)
+template<typename T>
+void out_arr(T* arr, int len)
 {
 	for (int i = 0; i < len; i++)
 	{
@@ -54,7 +27,8 @@ void out_arr(int* arr, int len)
 	}
 }
 //bubble sorting
-void insert_sort(int* arr, int len)
+template<typename T>
+void insert_sort(T* arr, int len)
 {
 	int count = 0, change = 0;
 
@@ -76,20 +50,74 @@ void insert_sort(int* arr, int len)
 	cout << "Insert sorting:\n";
 	cout << "Count of operations: " << count << endl;
 	cout << "Count of change: " << change << endl;
-	
+
 }
 
-void merge_sort(int* arr, int len)
+template <typename T>
+void merge(T* arr, int left, int right, int mid)
 {
-	int count = 0, change = 0;
-	for (int i = 0; i < len; i++)
-	{
-		
+	int j = left;
+	int k = mid + 1;
+	int len = right - left + 1;
+
+	if (len <= 1) return;
+
+	T* tmp = new T[len];
+
+	for (int i = 0; i < len; ++i) {
+		if (j <= mid && k <= right) {
+			if (arr[j] < arr[k])
+				tmp[i] = arr[j++];
+			else
+				tmp[i] = arr[k++];
+		}
+		else {
+			if (j <= mid)
+				tmp[i] = arr[j++];
+			else
+				tmp[i] = arr[k++];
+		}
 	}
-	cout << "Buble sorting, count of operations: " << count << endl;
-	for (int i = 0; i < len; i++)
-	{
-		cout << arr[i] << " ";
+
+	j = 0;
+	for (int i = left; i <= right; ++i) {
+		arr[i] = tmp[j++];
 	}
+	delete[] tmp;
+}
+
+// A function of the rising of the merger
+template <typename T>
+void merge_sort(T* arr, int l, int r)
+{
+	if (l < r) {
+		int m = l + (r - l) / 2;
+
+		merge_sort(arr, l, m);
+		merge_sort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
+	}
+}
+//sort insert and sort merge
+int main()
+{
+	srand(time_t(NULL));
+	int len;
+	cout << "Input size of array: ";
+	cin >> len;
+	int* arr = new int[len];
+
+	generate_arr(arr, len);
+
+	insert_sort(arr, len);
+
+	out_arr(arr, len);
 	cout << "\n\n";
+	merge_sort(arr, 1, len+1);
+
+	out_arr(arr, len);
+	cout << "\n";
+	delete[] arr;
+	return 0;
 }
